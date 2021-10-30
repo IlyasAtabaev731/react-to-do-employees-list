054538c061f00bb1d333bc69b2d46794b8a81917
Ilyas Atabaev <illegaldude7@gmail.com>
2021-10-31 03:12:25 +0500

    structure add

---
+++ src/components/search-panel/search-panel.jsx
@@ -0,0 +1,11 @@
+import './search-panel.css'
+
+function SearchPanel() {
+    return (
+        <input className="form-control search-input"
+        type="text"
+        placeholder="Найти сотрудника"
+        />
+    )
+}
+export default SearchPanel;
